4858f35a64438f93bb2ed84b3b4ff4ae3261f5a6
Kedar Panchal <kedar.t.panchal@outlook.com>
2023-09-23 10:30:11 -0500

    Added speed modification and prettied README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # SimpleRecorder
 A simple screen recording tool because everything else was too complicated for me.  
 
-Click the record button to record your mouse and keyboard movements. Press whatever key your inputted in the End Recording Key text box to end the recording. Enter the number of times you want to run the recording under the Number of Executions text box. Click run to run the recordings that number of times.  
+Click the record button to record your mouse and keyboard movements. Press whatever key your inputted in the End Recording Key text box to end the recording. Enter the number of times you want to run the recording under the Number of Executions text box. Input the speed at which you want the recording to run in the Speed text box. Speeds of less than 1 run slower than the original recording, and speeds of greater than 1 run faster.
+
+Click run to run the recordings at the number of times and speed specified.  
 
 Click Exit to exit the program, and click Clear Recording to reset the recording to a blank slate.
--- src/main.py
+++ src/main.py
@@ -3,15 +3,17 @@ import PySimpleGUI as gui
 
 gui.theme("DarkGreen2")
 layout = [
-    [gui.Text("End Recording Key"), gui.InputText(), gui.Button("Record")],
-    [gui.Text("No recording", key="RECORD_OUTPUT")],
+    [gui.Text("End Recording Key: "), gui.InputText()],
+    [gui.Push(), gui.Button("Record"), gui.Text("No recording", key="RECORD_OUTPUT"), gui.Push()],
     [gui.HorizontalSeparator(pad=(10, 10))],
-    [gui.Text("Number of Executions"), gui.InputText(), gui.Button("Run")],
-    [gui.Text("Inactive", key="ACTIVITY")],
-    [gui.Button("Exit"), gui.Button("Clear Recording")]
+    [gui.Text("Number of Executions: "), gui.InputText()],
+    [gui.Push(), gui.Text("Speed: "), gui.InputText()],
+    [gui.Push(), gui.Button("Run"), gui.Text("Inactive", key="ACTIVITY"), gui.Push()],
+    [gui.HorizontalSeparator(pad=(10, 10))],
+    [gui.Push(), gui.Button("Exit"), gui.Button("Clear Recording"), gui.Push()]
 ]
 
-window = gui.Window(title="Simple Recorder", layout=layout, element_justification='c')
+window = gui.Window(title="Simple Recorder", layout=layout, element_justification='l')
 
 mouse_inputs = []
 keyboard_inputs = []
@@ -33,9 +35,10 @@ while True:
         window["RECORD_OUTPUT"].update("No recording")
     elif event == "Run":
         try:
+            speed = float(values[3]) 
             window["ACTIVITY"].update("Running...")
             for i in range((1 if (not values[2]) else int(values[2]))):
-                play_inputs(mouse_events=mouse_inputs, keyboard_events=keyboard_inputs)
+                play_inputs(mouse_events=mouse_inputs, keyboard_events=keyboard_inputs, speed=speed)
             window["ACTIVITY"].update("Execution completed")
         except ValueError:
             play_inputs(mouse_events=mouse_inputs, keyboard_events=keyboard_inputs)
