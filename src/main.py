e858b5d61e0b36e59c4e144b823684a917e1d11f
Kedar Panchal <kedar.t.panchal@outlook.com>
2023-10-21 15:55:01 -0500

    Revert "Refactored code to have less bugs and make it more eye-pleasing"
    This reverts commit 401905e67bae1f661077561aef98e905ce238a3f.
    
    Revert commit back to original working multithreading code

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
+from lib import *
 import PySimpleGUI as gui
-from threading import Thread
-from lib import events_fn, record_fn
+import mouse
+import keyboard
 
-def main() -> None:
-    gui.theme("DarkGreen2")
-    layout = [
-        [gui.Text("End Recording Key: "), gui.InputText()],
-        [gui.Push(), gui.Button("Record"), gui.Text("No recording", key="RECORD_OUTPUT"), gui.Push()],
-        [gui.HorizontalSeparator(pad=(10, 10))],
-        [gui.Text("Number of Executions: "), gui.InputText()],
-        [gui.Push(), gui.Text("Speed: "), gui.InputText()],
-        [gui.Push(), gui.Button("Run"), gui.Text("Inactive", key="ACTIVITY"), gui.Push()],
-        [gui.HorizontalSeparator(pad=(10, 10))],
-        [gui.Push(), gui.Button("Exit"), gui.Button("Clear Recording"), gui.Push()]
-    ]
+gui.theme("DarkGreen2")
+layout = [
+    [gui.Text("End Recording Key: "), gui.InputText()],
+    [gui.Push(), gui.Button("Record"), gui.Text("No recording", key="RECORD_OUTPUT"), gui.Push()],
+    [gui.HorizontalSeparator(pad=(10, 10))],
+    [gui.Text("Number of Executions: "), gui.InputText()],
+    [gui.Push(), gui.Text("Speed: "), gui.InputText()],
+    [gui.Push(), gui.Button("Run"), gui.Text("Inactive", key="ACTIVITY"), gui.Push()],
+    [gui.HorizontalSeparator(pad=(10, 10))],
+    [gui.Push(), gui.Button("Exit"), gui.Button("Clear Recording"), gui.Push()]
+]
 
 window = gui.Window(title="Simple Recorder", layout=layout, element_justification='l', icon="C:/Users/kedar/00_Kedar/Python Projects/SimpleRecorder/out/imgs/SimpleRecorder.ico", titlebar_icon="C:/Users/kedar/00_Kedar/Python Projects/SimpleRecorder/out/imgs/SimpleRecorder.ico")
 
